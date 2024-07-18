399f24131d55970dfd5e76c1148f1da193a3e51d
Taoning Ge <gtn1024@foxmail.com>
2024-07-18 21:06:57 +0800

    add codestyle config

--- my-platform/src/main/java/net/ximatai/muyun/platform/controller/ModuleController.java
+++ my-platform/src/main/java/net/ximatai/muyun/platform/controller/ModuleController.java
@@ -1,9 +1,6 @@
 package net.ximatai.muyun.platform.controller;
 
-
-import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.QueryParam;
 import net.ximatai.muyun.core.ability.curd.ICURDAbility;
 
 @Path("/module")
