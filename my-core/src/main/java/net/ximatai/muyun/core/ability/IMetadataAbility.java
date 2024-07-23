5cb0abcb4f433245ed53641eb14d7fca00a9afeb
Liu Rui <lovearuis@gmail.com>
2024-07-23 17:31:44 +0800

    format code

--- my-core/src/main/java/net/ximatai/muyun/core/ability/IMetadataAbility.java
+++ my-core/src/main/java/net/ximatai/muyun/core/ability/IMetadataAbility.java
@@ -7,24 +7,24 @@ public interface IMetadataAbility {
 
     String getMainTable();
 
-    default String getSelectOneRowSql(){
+    default String getSelectOneRowSql() {
         return "select * from " + getMainTable() + " where id=:id";
     }
 
-    default String getInsertSql(Map<String, Object> params){
-        StringJoiner columns = new StringJoiner(", ","(",")");
-        StringJoiner values = new StringJoiner(", ","(",")");
+    default String getInsertSql(Map<String, Object> params) {
+        StringJoiner columns = new StringJoiner(", ", "(", ")");
+        StringJoiner values = new StringJoiner(", ", "(", ")");
         params.keySet().forEach(key -> {
             columns.add(key);
-            values.add(":"+key);
+            values.add(":" + key);
         });
 
         return "INSERT INTO " + getMainTable() + " " + columns + " VALUES " + values + " ";
     }
 
-    default String getUpdateSql(Map<String, Object> params){
+    default String getUpdateSql(Map<String, Object> params) {
         StringJoiner setClause = new StringJoiner(", ");
-        params.keySet().forEach(key->{
+        params.keySet().forEach(key -> {
             setClause.add(key + "=:" + key);
         });
         return "UPDATE " + getMainTable() + " SET " + setClause + " WHERE id=:id";
