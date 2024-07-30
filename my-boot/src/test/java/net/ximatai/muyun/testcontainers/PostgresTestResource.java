3b28d71a1f9f6b55a269b7f7127c0a32723941ff
Liu Rui <lovearuis@gmail.com>
2024-07-30 22:03:04 +0800

    chore:优化pg镜像名

--- my-boot/src/test/java/net/ximatai/muyun/testcontainers/PostgresTestResource.java
+++ my-boot/src/test/java/net/ximatai/muyun/testcontainers/PostgresTestResource.java
@@ -7,17 +7,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PostgresTestResource implements QuarkusTestResourceLifecycleManager {
-    PostgreSQLContainer<?> pgvector;
+    PostgreSQLContainer<?> postgres;
 
     @Override
     public Map<String, String> start() {
-        pgvector = new PostgreSQLContainer<>("pgvector/pgvector:pg16");
-        pgvector.start();
+        postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+        postgres.start();
 
         Map<String, String> conf = new HashMap<>();
-        conf.put("quarkus.datasource.jdbc.url", pgvector.getJdbcUrl());
-        conf.put("quarkus.datasource.username", pgvector.getUsername());
-        conf.put("quarkus.datasource.password", pgvector.getPassword());
+        conf.put("quarkus.datasource.jdbc.url", postgres.getJdbcUrl());
+        conf.put("quarkus.datasource.username", postgres.getUsername());
+        conf.put("quarkus.datasource.password", postgres.getPassword());
         conf.put("quarkus.hibernate-orm.database.generation", "create");
 
         return conf;
@@ -25,6 +25,6 @@ public class PostgresTestResource implements QuarkusTestResourceLifecycleManager
 
     @Override
     public void stop() {
-        pgvector.stop();
+        postgres.stop();
     }
 }
