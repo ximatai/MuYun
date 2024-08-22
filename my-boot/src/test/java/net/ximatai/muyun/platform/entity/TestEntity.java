7d2e9e6ea660c8d7defaeadda87bb894100ea0d1
Liu Rui <lovearuis@gmail.com>
2024-08-22 12:38:50 +0800

    style-STD测试用例-调整TestEntity的import语句

--- my-boot/src/test/java/net/ximatai/muyun/platform/entity/TestEntity.java
+++ my-boot/src/test/java/net/ximatai/muyun/platform/entity/TestEntity.java
@@ -1,6 +1,10 @@
 package net.ximatai.muyun.platform.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
