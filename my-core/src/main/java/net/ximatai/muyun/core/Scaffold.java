e6b20d106d33e32bb853a5af3c0df55985ae0af2
Liu Rui <lovearuis@gmail.com>
2024-09-19 13:36:31 +0800

    feat-IDataBroadcastAbility-优化逻辑到Scaffold中简化业务代码

--- my-core/src/main/java/net/ximatai/muyun/core/Scaffold.java
+++ my-core/src/main/java/net/ximatai/muyun/core/Scaffold.java
@@ -1,5 +1,6 @@
 package net.ximatai.muyun.core;
 
+import io.vertx.core.eventbus.EventBus;
 import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
 import net.ximatai.muyun.ability.ITableCreateAbility;
@@ -8,14 +9,24 @@ import net.ximatai.muyun.database.IDatabaseAccess;
 public class Scaffold {
 
     private IDatabaseAccess databaseAccess;
+    private EventBus eventBus;
 
     @Inject
     public void setDatabaseAccess(IDatabaseAccess databaseAccess) {
         this.databaseAccess = databaseAccess;
     }
 
+    @Inject
+    public void setEventBus(EventBus eventBus) {
+        this.eventBus = eventBus;
+    }
+
     public IDatabaseAccess getDatabaseAccess() {
         return databaseAccess;
+    }
+
+    public EventBus getEventBus() {
+        return eventBus;
     }
 
     @PostConstruct
