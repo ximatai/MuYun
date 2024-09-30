875d8a882d60428d0bf6c54d35f50ce42a1033b1
Liu Rui <lovearuis@gmail.com>
2024-09-30 10:40:29 +0800

    feat-MenuSchemaController-提供根据用户获取对应方案的方法

--- muyun-platform/src/main/java/net/ximatai/muyun/platform/controller/MenuSchemaController.java
+++ muyun-platform/src/main/java/net/ximatai/muyun/platform/controller/MenuSchemaController.java
@@ -4,8 +4,10 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.QueryParam;
 import net.ximatai.muyun.ability.IChildrenAbility;
 import net.ximatai.muyun.ability.ISortAbility;
+import net.ximatai.muyun.core.exception.MyException;
 import net.ximatai.muyun.database.builder.Column;
 import net.ximatai.muyun.database.builder.TableWrapper;
 import net.ximatai.muyun.model.ChildTableInfo;
@@ -13,9 +15,11 @@ import net.ximatai.muyun.model.TreeNode;
 import net.ximatai.muyun.platform.ScaffoldForPlatform;
 import net.ximatai.muyun.platform.model.Dict;
 import net.ximatai.muyun.platform.model.DictCategory;
+import net.ximatai.muyun.service.IAuthorizationService;
 import net.ximatai.muyun.util.TreeBuilder;
 
 import java.util.List;
+import java.util.Map;
 
 import static net.ximatai.muyun.platform.PlatformConst.BASE_PATH;
 
@@ -27,6 +31,9 @@ public class MenuSchemaController extends ScaffoldForPlatform implements IChildr
 
     @Inject
     MenuController menuController;
+
+    @Inject
+    IAuthorizationService authorizationService;
 
     @Override
     protected void afterInit() {
@@ -66,5 +73,38 @@ public class MenuSchemaController extends ScaffoldForPlatform implements IChildr
     public List<TreeNode> tree(@PathParam("id") String id) {
         List menus = getChildTableList(id, "app_menu", null);
         return TreeBuilder.build("id", "pid", menus, null, false, "v_name", null);
+    }
+
+    @GET
+    @Path("/schemaForUser")
+    public String schemaForUser(@QueryParam("userID") String userID, @QueryParam("terminalType") String terminalType) {
+        if (terminalType == null) {
+            terminalType = "web";
+        }
+        List<String> roles = authorizationService.getUserAvailableRoles(userID);
+
+        Map row = getDB().row("""
+            select * from platform.app_menu_schema 
+            where ? = any(dicts_terminal_type)
+            and ids_at_auth_role && ?
+            order by n_order limit 1
+            """, terminalType, getDB().toArray(roles, "VARCHAR"));
+
+        if (row != null) {
+            return row.get("id").toString();
+        }
+
+        row = getDB().row("""
+            select * from platform.app_menu_schema 
+            where ? = any(dicts_terminal_type)
+            and (select id_at_org_organization from platform.auth_userinfo where id = ?) = any(ids_at_orga_organization)
+            order by n_order limit 1
+            """, terminalType, userID);
+
+        if (row != null) {
+            return row.get("id").toString();
+        } else {
+            throw new MyException("未找到与用户[%s]匹配的菜单方案");
+        }
     }
 }
