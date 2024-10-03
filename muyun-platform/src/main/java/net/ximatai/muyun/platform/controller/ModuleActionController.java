6bd946cd5a921a12f59d268ca54cd141a96e53e8
Liu Rui <lovearuis@gmail.com>
2024-10-03 10:41:22 +0800

    feat-ModuleActionController-模块及功能删除时，删除对应的权限

--- muyun-platform/src/main/java/net/ximatai/muyun/platform/controller/ModuleActionController.java
+++ muyun-platform/src/main/java/net/ximatai/muyun/platform/controller/ModuleActionController.java
@@ -1,16 +1,22 @@
 package net.ximatai.muyun.platform.controller;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import net.ximatai.muyun.ability.IChildAbility;
+import net.ximatai.muyun.ability.IChildrenAbility;
 import net.ximatai.muyun.database.builder.Column;
 import net.ximatai.muyun.database.builder.TableWrapper;
+import net.ximatai.muyun.model.ChildTableInfo;
 import net.ximatai.muyun.model.SortColumn;
 import net.ximatai.muyun.platform.ScaffoldForPlatform;
 
 import java.util.List;
 
 @ApplicationScoped
-public class ModuleActionController extends ScaffoldForPlatform implements IChildAbility {
+public class ModuleActionController extends ScaffoldForPlatform implements IChildAbility, IChildrenAbility {
+
+    @Inject
+    RoleActionController roleActionController;
 
     @Override
     public SortColumn getDefatultSortColumn() {
@@ -35,4 +41,10 @@ public class ModuleActionController extends ScaffoldForPlatform implements IChil
             .addIndex(List.of("id_at_app_module", "v_alias"), true);
     }
 
+    @Override
+    public List<ChildTableInfo> getChildren() {
+        return List.of(
+            roleActionController.toChildTable("id_at_app_module_action").setAutoDelete()
+        );
+    }
 }
