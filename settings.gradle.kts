62b0fcd66bf54d4f007a3f4b1a1149ea7e6294f9
Liu Rui <lovearuis@gmail.com>
2024-10-04 23:56:41 +0800

    build-pluginManagement-暂时只保留官方插件

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement {
     repositories {
-        maven { url = uri("https://mirrors.cloud.tencent.com/repository/gradle-plugins/") }
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+//        maven { url = uri("https://mirrors.cloud.tencent.com/repository/gradle-plugins/") }
+//        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
         gradlePluginPortal()  // 保留官方 Gradle 插件门户
     }
 }
