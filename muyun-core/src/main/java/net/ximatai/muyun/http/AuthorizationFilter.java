d7bbdf3a44f3dbf31844e24919c7dd099ea56691
Liu Rui <lovearuis@gmail.com>
2024-10-08 15:44:02 +0800

    feat-debug模式下允许跨域访问

--- muyun-core/src/main/java/net/ximatai/muyun/http/AuthorizationFilter.java
+++ muyun-core/src/main/java/net/ximatai/muyun/http/AuthorizationFilter.java
@@ -1,6 +1,7 @@
 package net.ximatai.muyun.http;
 
 import io.quarkus.vertx.web.RouteFilter;
+import io.vertx.core.MultiMap;
 import io.vertx.ext.web.RoutingContext;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Instance;
@@ -8,6 +9,7 @@ import jakarta.inject.Inject;
 import net.ximatai.muyun.MuYunConst;
 import net.ximatai.muyun.RouterFilterPriority;
 import net.ximatai.muyun.ability.IRuntimeAbility;
+import net.ximatai.muyun.core.MuYunConfig;
 import net.ximatai.muyun.model.ApiRequest;
 import net.ximatai.muyun.model.IRuntimeUser;
 import net.ximatai.muyun.service.IAuthorizationService;
@@ -25,11 +27,23 @@ public class AuthorizationFilter implements IRuntimeAbility {
     @Inject
     Instance<IAuthorizationService> authorizationService;
 
+    @Inject
+    MuYunConfig config;
+
     @RouteFilter(RouterFilterPriority.AUTHORIZATION)
     void filter(RoutingContext context) {
         String path = context.request().path();
 
         if (path.startsWith(restPath)) { //仅对 /api开头的请求做权限拦截
+
+            if (config.debug()) {
+                MultiMap headers = context.response().headers();
+                headers.add("Access-Control-Allow-Origin", "*");
+                headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, OPTIONS");
+                headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization");
+                headers.add("Access-Control-Expose-Headers", "Content-Disposition");
+            }
+
             IRuntimeUser runtimeUser = this.getUser();
             ApiRequest apiRequest = new ApiRequest(runtimeUser, path);
 
