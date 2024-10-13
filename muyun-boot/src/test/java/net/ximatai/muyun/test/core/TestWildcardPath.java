b745b1a8da2bfba4977699c4a2fb022792771245
Liu Rui <lovearuis@gmail.com>
2024-10-13 14:29:35 +0800

    test-TestWildcardPath-修正github CI不通过的问题

--- muyun-boot/src/test/java/net/ximatai/muyun/test/core/TestWildcardPath.java
+++ muyun-boot/src/test/java/net/ximatai/muyun/test/core/TestWildcardPath.java
@@ -1,13 +1,16 @@
 package net.ximatai.muyun.test.core;
 
+import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import net.ximatai.muyun.test.testcontainers.PostgresTestResource;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
 @QuarkusTest
+@QuarkusTestResource(value = PostgresTestResource.class, restrictToAnnotatedClass = true)
 public class TestWildcardPath {
 
     @Test
