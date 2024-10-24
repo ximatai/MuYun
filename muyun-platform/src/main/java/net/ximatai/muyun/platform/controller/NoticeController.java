43d437288f3bcac5901f002ff970c705ce886d4e
Liu Rui <lovearuis@gmail.com>
2024-10-24 08:50:19 +0800

    feat-通知发布/通知接收-相关功能实现

---
+++ muyun-platform/src/main/java/net/ximatai/muyun/platform/controller/NoticeController.java
@@ -0,0 +1,80 @@
+package net.ximatai.muyun.platform.controller;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import net.ximatai.muyun.base.BaseBusinessTable;
+import net.ximatai.muyun.database.builder.Column;
+import net.ximatai.muyun.database.builder.TableWrapper;
+import net.ximatai.muyun.platform.ScaffoldForPlatform;
+import net.ximatai.muyun.platform.model.Dict;
+import net.ximatai.muyun.platform.model.DictCategory;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import static net.ximatai.muyun.platform.PlatformConst.BASE_PATH;
+
+@Tag(name = "公告发布")
+@Path(BASE_PATH + "/notice")
+public class NoticeController extends ScaffoldForPlatform {
+    @Inject
+    DictCategoryController dictCategoryController;
+
+    @Override
+    public String getMainTable() {
+        return "app_notice";
+    }
+
+    @Override
+    protected void afterInit() {
+        dictCategoryController.putDictCategory(
+            new DictCategory("dict_notice_access_scope", "platform_dir", "通知公开范围", 0).setDictList(
+                new Dict("open", "全公开"),
+                new Dict("organization", "按机构"),
+                new Dict("department", "按部门")
+            ), false);
+    }
+
+    @Override
+    public void fitOut(TableWrapper wrapper) {
+        wrapper
+            .setComment("通知公告")
+            .setPrimaryKey(Column.ID_POSTGRES)
+            .setInherit(BaseBusinessTable.TABLE)
+            .addColumn("v_title", "标题")
+            .addColumn("v_context", "内容")
+            .addColumn("file_att", "附件")
+            .addColumn("i_views", "浏览量", 0)
+            .addColumn("dict_notice_access_scope", "公开范围")
+            .addColumn("ids_at_org_organization", "公开机构范围")
+            .addColumn("ids_at_org_department", "公开部门范围")
+            .addColumn("b_release", "是否发布", false)
+            .addColumn("t_release", "发布时间");
+    }
+
+    @GET
+    @Path("/release/{id}")
+    @Operation(summary = "发布公告")
+    public int release(@PathParam("id") String id) {
+        return getDB().updateItem(getSchemaName(), getMainTable(), Map.of(
+            "id", id,
+            "b_release", true,
+            "t_release", LocalDateTime.now()
+        ));
+    }
+
+    @GET
+    @Path("/rollback/{id}")
+    @Operation(summary = "撤销发布公告")
+    public int rollback(@PathParam("id") String id) {
+        return getDB().updateItem(getSchemaName(), getMainTable(), Map.of(
+            "id", id,
+            "b_release", false
+        ));
+    }
+
+}
---
+++ muyun-platform/src/main/java/net/ximatai/muyun/platform/controller/ReceiveController.java
@@ -0,0 +1,62 @@
+package net.ximatai.muyun.platform.controller;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Path;
+import net.ximatai.muyun.ability.IReferenceAbility;
+import net.ximatai.muyun.ability.curd.std.ISelectAbility;
+import net.ximatai.muyun.core.Scaffold;
+import net.ximatai.muyun.model.IRuntimeUser;
+import net.ximatai.muyun.model.ReferenceInfo;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import java.util.List;
+import java.util.Map;
+
+import static net.ximatai.muyun.platform.PlatformConst.BASE_PATH;
+import static net.ximatai.muyun.platform.PlatformConst.DB_SCHEMA;
+
+@Tag(name = "公告接收")
+@Path(BASE_PATH + "/receive")
+public class ReceiveController extends Scaffold implements ISelectAbility, IReferenceAbility {
+
+    @Inject
+    UserInfoController userInfoController;
+
+    @Override
+    public String getMainTable() {
+        return "app_notice";
+    }
+
+    @Override
+    public String getSchemaName() {
+        return DB_SCHEMA;
+    }
+
+    @Override
+    public Map<String, ?> view(String id) {
+        Map<String, ?> view = ISelectAbility.super.view(id);
+        if (view != null) {
+            getDB().update("update %s.%s set i_views = i_views + 1 where id = ?".formatted(getSchemaName(), getMainTable()), id);
+        }
+        return view;
+    }
+
+    @Override
+    public List<ReferenceInfo> getReferenceList() {
+        return List.of(
+            userInfoController.toReferenceInfo("id_at_auth_user__create")
+        );
+    }
+
+    @Override
+    public String getAuthCondition() {
+        IRuntimeUser user = getUser();
+        return """
+             and b_release = true
+             and (dict_notice_access_scope = 'open'
+                 or (dict_notice_access_scope='organization' and '%s' = any(ids_at_org_organization))
+                 or (dict_notice_access_scope='department' and '%s' = any(ids_at_org_department))
+                 )
+            """.formatted(user.getOrganizationId(), user.getDepartmentId());
+    }
+}
