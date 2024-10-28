c65bab0653c2a417dfe4468be51229610cc97abf
Liu Rui <lovearuis@gmail.com>
2024-10-28 17:20:37 +0800

    feat-FileServerConfig-给pagePath一个默认值

--- muyun-fileserver/src/main/java/net/ximatai/muyun/fileserver/FileServerConfig.java
+++ muyun-fileserver/src/main/java/net/ximatai/muyun/fileserver/FileServerConfig.java
@@ -1,10 +1,12 @@
 package net.ximatai.muyun.fileserver;
 
 import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
 
 @ConfigMapping(prefix = "file-server")
 public interface FileServerConfig {
     String uploadPath();
-    
+
+    @WithDefault("fileServer")
     String pagePath();
 }
