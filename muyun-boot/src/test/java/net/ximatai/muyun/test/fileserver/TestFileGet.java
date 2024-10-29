9748407651cd21a32609eae2da468783b1d2e52d
maning <3297813122@qq.com>
2024-10-29 09:10:54 +0800

    fix-fileServer-修正java层get文件存在uid后缀问题，加入单元测试

--- muyun-boot/src/test/java/net/ximatai/muyun/test/fileserver/TestFileGet.java
+++ muyun-boot/src/test/java/net/ximatai/muyun/test/fileserver/TestFileGet.java
@@ -9,7 +9,6 @@ import net.ximatai.muyun.test.testcontainers.PostgresTestResource;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
