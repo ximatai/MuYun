b2ae7ab82763abb193b2ccc7fdac55d2a49dc116
Liu Rui <lovearuis@gmail.com>
2024-11-20 15:13:05 +0800

    docs-readme

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 allprojects {
     group = "net.ximatai.muyun"
     version = "1.0.0-SNAPSHOT"
-//    version = "0.1.5"
+//    version = "0.1.6"
 
     repositories {
         mavenLocal()
