acee64c54d509681b2455b8e66857272bdbac883
6greenhand <2099769588@qq.com>
2024-12-03 17:01:32 +0800

    docs-util-补充DisplayName

--- muyun-boot/src/test/java/net/ximatai/muyun/test/util/TreeBuilderTest.java
+++ muyun-boot/src/test/java/net/ximatai/muyun/test/util/TreeBuilderTest.java
@@ -2,6 +2,7 @@ package net.ximatai.muyun.test.util;
 
 import net.ximatai.muyun.model.TreeNode;
 import net.ximatai.muyun.util.TreeBuilder;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -25,6 +26,7 @@ class TreeBuilderTest {
     );
 
     @Test
+    @DisplayName("测试构建整棵树且不显示根节点")
     void testTree() {
         List<TreeNode> tree = TreeBuilder.build("id", "pid", list, null, false, "name", 10);
         TreeNode nodeA = tree.get(0);
@@ -34,6 +36,7 @@ class TreeBuilderTest {
     }
 
     @Test
+    @DisplayName("测试构建以'A'为根节点的树但显示根节点")
     void testTreeA() {
         List<TreeNode> tree = TreeBuilder.build("id", "pid", list, "A", true, "name", 10);
         TreeNode nodeA = tree.get(0);
@@ -43,6 +46,7 @@ class TreeBuilderTest {
     }
 
     @Test
+    @DisplayName("测试构建以'A'为根节点且不显示根节点")
     void testTreeANotShowMe() {
         List<TreeNode> tree = TreeBuilder.build("id", "pid", list, "A", false, "name", 10);
         assertEquals(2, tree.size());
@@ -50,6 +54,7 @@ class TreeBuilderTest {
     }
 
     @Test
+    @DisplayName("测试构建不存在的根节点")
     void testTreeNotFound() {
         List<TreeNode> tree = TreeBuilder.build("id", "pid", list, "X", false, "name", 10);
         assertNull(tree);
