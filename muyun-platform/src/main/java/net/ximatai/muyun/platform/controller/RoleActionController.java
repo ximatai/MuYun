24ff7364944cd18764a8ead67e24212510835acc
Liu Rui <lovearuis@gmail.com>
2024-12-14 11:29:42 +0800

    feat-RoleActionController-优化索引

--- muyun-platform/src/main/java/net/ximatai/muyun/platform/controller/RoleActionController.java
+++ muyun-platform/src/main/java/net/ximatai/muyun/platform/controller/RoleActionController.java
@@ -50,8 +50,7 @@ public class RoleActionController extends ScaffoldForPlatform implements IChildA
             .addColumn("b_use", "是否启用", true)
             .addIndex("id_at_auth_role")
             .addIndex("id_at_app_module_action")
-            .addIndex(List.of("id_at_auth_role", "id_at_app_module_action"), true)
-            .addIndex(List.of("id_at_auth_role", "v_alias_at_app_module", "v_alias_at_app_module_action"), true);
+            .addIndex(List.of("id_at_auth_role", "id_at_app_module_action"), true);
     }
 
     @Override
