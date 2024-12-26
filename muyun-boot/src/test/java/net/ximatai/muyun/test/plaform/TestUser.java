f946b6015f7dff9bdd55c46e585d778d50946f65
6greenhand <2099769588@qq.com>
2024-12-26 16:29:23 +0800

    feat-密码复杂度单元测试

--- muyun-boot/src/test/java/net/ximatai/muyun/test/plaform/TestUser.java
+++ muyun-boot/src/test/java/net/ximatai/muyun/test/plaform/TestUser.java
@@ -34,6 +34,198 @@ public class TestUser {
     RoleController roleController;
 
     @Test
+    @DisplayName("测试密码复杂度1")
+    void testPasswordComplexity1() {
+        //新增用户
+        String id = given()
+            .header("userID", config.superUserId())
+            .contentType("application/json")
+            .body(Map.of(
+                "v_name", "测试",
+                "dict_user_gender", "0"
+            ))
+            .when()
+            .post("/api%s/userinfo/create".formatted(base))
+            .then()
+            .statusCode(200)
+            .extract()
+            .asString();
+        // 设置用户
+        given()
+            .header("userID", config.superUserId())
+            .contentType("application/json")
+            .body(Map.of(
+                "v_username", "test",
+                "v_password", "pw",
+                "v_password2", "pw",
+                "roles", List.of("1", "2")
+            ))
+            .when()
+            .post("/api%s/userinfo/setUser/%s".formatted(base, id))
+            .then()
+            .statusCode(200)
+            .extract()
+            .asString();
+        // 修改密码
+        given()
+            .header("userID", config.superUserId())
+            .contentType("application/json")
+            .body(Map.of(
+                "v_password", "12345z",
+                "v_password2", "12345z"
+            ))
+            .when()
+            .post("/api%s/userinfo/setPassword/%s".formatted(base, id))
+            .then()
+            .statusCode(500)
+            .extract()
+            .asString();
+        // 修改密码（自助）
+        given()
+            .header("userID", config.superUserId())
+            .contentType("application/json")
+            .body(Map.of(
+                "v_old_password", "pw",
+                "v_password", "12345z",
+                "v_password2", "12345z"
+            ))
+            .when()
+            .post("/api%s/userinfo/setPasswordSelf/%s".formatted(base, id))
+            .then()
+            .statusCode(500)
+            .extract()
+            .asString();
+    }
+
+    @Test
+    @DisplayName("测试密码复杂度2")
+    void testPasswordComplexity2() {
+        //新增用户
+        String id = given()
+            .header("userID", config.superUserId())
+            .contentType("application/json")
+            .body(Map.of(
+                "v_name", "测试",
+                "dict_user_gender", "0"
+            ))
+            .when()
+            .post("/api%s/userinfo/create".formatted(base))
+            .then()
+            .statusCode(200)
+            .extract()
+            .asString();
+        // 设置用户
+        given()
+            .header("userID", config.superUserId())
+            .contentType("application/json")
+            .body(Map.of(
+                "v_username", "test",
+                "v_password", "pw",
+                "v_password2", "pw",
+                "roles", List.of("1", "2")
+            ))
+            .when()
+            .post("/api%s/userinfo/setUser/%s".formatted(base, id))
+            .then()
+            .statusCode(200)
+            .extract()
+            .asString();
+        // 修改密码
+        given()
+            .header("userID", config.superUserId())
+            .contentType("application/json")
+            .body(Map.of(
+                "v_password", "12345678",
+                "v_password2", "12345678"
+            ))
+            .when()
+            .post("/api%s/userinfo/setPassword/%s".formatted(base, id))
+            .then()
+            .statusCode(500)
+            .extract()
+            .asString();
+        // 修改密码（自助）
+        given()
+            .header("userID", config.superUserId())
+            .contentType("application/json")
+            .body(Map.of(
+                "v_old_password", "pw",
+                "v_password", "12345678",
+                "v_password2", "12345678"
+            ))
+            .when()
+            .post("/api%s/userinfo/setPasswordSelf/%s".formatted(base, id))
+            .then()
+            .statusCode(500)
+            .extract()
+            .asString();
+    }
+
+    @Test
+    @DisplayName("测试密码复杂度3")
+    void testPasswordComplexity3() {
+        //新增用户
+        String id = given()
+            .header("userID", config.superUserId())
+            .contentType("application/json")
+            .body(Map.of(
+                "v_name", "测试",
+                "dict_user_gender", "0"
+            ))
+            .when()
+            .post("/api%s/userinfo/create".formatted(base))
+            .then()
+            .statusCode(200)
+            .extract()
+            .asString();
+        // 设置用户
+        given()
+            .header("userID", config.superUserId())
+            .contentType("application/json")
+            .body(Map.of(
+                "v_username", "test",
+                "v_password", "pw",
+                "v_password2", "pw",
+                "roles", List.of("1", "2")
+            ))
+            .when()
+            .post("/api%s/userinfo/setUser/%s".formatted(base, id))
+            .then()
+            .statusCode(200)
+            .extract()
+            .asString();
+        // 修改密码
+        given()
+            .header("userID", config.superUserId())
+            .contentType("application/json")
+            .body(Map.of(
+                "v_password", "123",
+                "v_password2", "123"
+            ))
+            .when()
+            .post("/api%s/userinfo/setPassword/%s".formatted(base, id))
+            .then()
+            .statusCode(500)
+            .extract()
+            .asString();
+        // 修改密码（自助）
+        given()
+            .header("userID", config.superUserId())
+            .contentType("application/json")
+            .body(Map.of(
+                "v_old_password", "pw",
+                "v_password", "123",
+                "v_password2", "123"
+            ))
+            .when()
+            .post("/api%s/userinfo/setPasswordSelf/%s".formatted(base, id))
+            .then()
+            .statusCode(500)
+            .extract()
+            .asString();
+    }
+
+    @Test
     @DisplayName("测试用户创建、设置、登录、停用、启用、修改密码和删除功能")
     void test() {
         roleController.create(Map.of("id", "1"));
@@ -49,6 +241,22 @@ public class TestUser {
             ))
             .when()
             .post("/api%s/userinfo/create".formatted(base))
+            .then()
+            .statusCode(200)
+            .extract()
+            .asString();
+        // 设置用户
+        given()
+            .header("userID", config.superUserId())
+            .contentType("application/json")
+            .body(Map.of(
+                "v_username", "test",
+                "v_password", "pw",
+                "v_password2", "pw",
+                "roles", List.of("1", "2")
+            ))
+            .when()
+            .post("/api%s/userinfo/setUser/%s".formatted(base, id))
             .then()
             .statusCode(200)
             .extract()
@@ -171,13 +379,13 @@ public class TestUser {
             .header("userID", config.superUserId())
             .contentType("application/json")
             .body(Map.of(
-                "v_password", "pw2",
-                "v_password2", "pw2"
+                "v_password", "1234567z",
+                "v_password2", "1234567z"
             ))
             .when()
             .post("/api%s/userinfo/setPassword/%s".formatted(base, id))
             .then()
-            .statusCode(500)
+            .statusCode(200)
             .extract()
             .asString();
 
