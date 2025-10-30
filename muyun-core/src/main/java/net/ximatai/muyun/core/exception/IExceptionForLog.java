e5c52d14f581a02d437e3e4850a488dfee525821
Liu Rui <lovearuis@gmail.com>
2025-10-30 21:48:59 +0800

    feat: IExceptionForLog

---
+++ muyun-core/src/main/java/net/ximatai/muyun/core/exception/IExceptionForLog.java
@@ -0,0 +1,7 @@
+package net.ximatai.muyun.core.exception;
+
+public interface IExceptionForLog {
+
+    String getLogMessage();
+
+}
--- muyun-core/src/main/java/net/ximatai/muyun/core/exception/MuYunException.java
+++ muyun-core/src/main/java/net/ximatai/muyun/core/exception/MuYunException.java
@@ -1,7 +1,12 @@
 package net.ximatai.muyun.core.exception;
 
-public class MuYunException extends RuntimeException implements IToFrontendException {
+public class MuYunException extends RuntimeException implements IToFrontendException, IExceptionForLog {
     public MuYunException(String message) {
         super(message);
+    }
+
+    @Override
+    public String getLogMessage() {
+        return this.getMessage();
     }
 }
