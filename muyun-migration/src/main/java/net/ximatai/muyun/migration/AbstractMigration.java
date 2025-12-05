f5e7a6e08223ba36f2f9127a3da066be149c77c0
Taoning Ge <gtn1024@foxmail.com>
2025-12-05 11:28:12 +0800

    feat: 增强迁移的日志

--- muyun-migration/src/main/java/net/ximatai/muyun/migration/AbstractMigration.java
+++ muyun-migration/src/main/java/net/ximatai/muyun/migration/AbstractMigration.java
@@ -6,7 +6,11 @@ import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 
 public abstract class AbstractMigration {
     protected final Logger logger = LoggerFactory.getLogger(getClass());
@@ -28,26 +32,34 @@ public abstract class AbstractMigration {
             throw new IllegalStateException("Duplicate migration version found in " + getAlias());
         }
 
-        migrateSteps.forEach((step) -> {
+        int beforeVersion = 0;
+        for (MigrateStep step : migrateSteps) {
             Map<String, Object> view = migrationController.view(getAlias());
-            Integer version;
+            Integer currentVersion;
             if (view == null) {
                 migrationController.create(Map.of(
                     "id", getAlias(),
                     "i_version", 0
                 ));
-                version = 0;
+                currentVersion = 0;
+                beforeVersion = 0;
             } else {
-                version = (Integer) view.get("i_version");
+                currentVersion = (Integer) view.get("i_version");
+                beforeVersion = currentVersion;
             }
-            if (version < step.version()) {
+            if (currentVersion < step.version()) {
                 logger.info("Migrating {} to version {}", getAlias(), step.version());
                 step.action().migrate();
                 migrationController.update(getAlias(), Map.of("i_version", step.version()));
+                logger.info("Migrated {} to version {}", getAlias(), step.version());
             }
-        });
+        }
 
         int finalVer = migrateSteps.isEmpty() ? 0 : migrateSteps.getLast().version();
-        logger.info("Migrated {} to {}", getAlias(), finalVer);
+        if (beforeVersion == finalVer) {
+            logger.info("Do not need migrate {} , current version is {}", getAlias(), beforeVersion);
+        } else {
+            logger.info("Migrated {} from {} to {}", getAlias(), beforeVersion, finalVer);
+        }
     }
 }
